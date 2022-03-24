91170f11e7614416ca7c918474956c578390261d
Tyler Freeman <fuego@google.com>
2022-03-24 17:25:05 +0000

    Add Tweaks to circleView

--- app/src/main/java/com/google/tweakr/sample/CircleView.kt
+++ app/src/main/java/com/google/tweakr/sample/CircleView.kt
@@ -19,23 +19,56 @@ import android.graphics.Color
 import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.View
+import com.google.tweakr.Tweakr
+import com.google.tweakr.TweakrRepo
+import com.google.tweakr.annotations.Tweak
 
 /** A View that draws a circle **/
 class CircleView @JvmOverloads constructor(
-  context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
-) : View(context, attrs, defStyleAttr) {
+  context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0,
+) : View(context, attrs, defStyleAttr), TweakrRepo.OnChangeListener {
+
+  @Tweak
   var centerX = 180f
+
+  @Tweak
   var centerY = 180f
+
+  @Tweak
   var radius = 50f
+
   var style = Paint.Style.FILL
   var color = Color.GREEN
 
   private val paint = Paint()
+
+  init {
+    Tweakr.register(this)
+  }
 
   override fun onDraw(canvas: Canvas) {
     super.onDraw(canvas)
     paint.style = style
     paint.color = color
     canvas.drawCircle(centerX, centerY, radius, paint)
+  }
+
+  override fun onAttachedToWindow() {
+    super.onAttachedToWindow()
+
+    // Register onFieldChanged() to redraw when any value changes.
+    Tweakr.addListener(this)
+  }
+
+  override fun onDetachedFromWindow() {
+    Tweakr.removeListener(this)
+    super.onDetachedFromWindow()
+  }
+
+  override fun onFieldChanged(name: String?, value: Any?) {
+    // This is called whenever a field's value is changed in Tweakr's UI. We could be granular
+    // here and check the name to match only the fields we care about, but for this demo it's
+    // simple enough to just redraw whenever any value changes.
+    invalidate()
   }
 }
